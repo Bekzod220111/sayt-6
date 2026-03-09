c27e19cd61a3db82a614b7c05d480ee22a863f7d
Bekzod <tobby4173@gmail.com>
2026-03-09 14:00:36 +0500

    yulduz

--- sayt/urls.py
+++ sayt/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index.html', views.index, name='index'),
+    path('', views.index, name='index'),
     path('blog.html', views.blog, name='blog'),
     path('blog-detail/', views.blog_detail, name='blog-detail'),
     path('contact/', views.contact, name='contact'),
